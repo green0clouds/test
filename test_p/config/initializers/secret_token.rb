83cb6d5f1ba275dd7d95e955694170104473cf8d
green0clouds <pishchulin@gmail.com>
2013-08-13 22:43:09 -0400

    first commit

---
+++ test_p/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestP::Application.config.secret_key_base = 'a8806df6bc3bb6fc12ff9e702b59e23575767c12ecae4b09dda7169b489eea5be607d731d6a1b94ce608d77e3421094bdab7cbe0f0a6a8eec8ce48a698009c14'
---
+++ test_p/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestP::Application.config.session_store :cookie_store, key: '_test_p_session'
